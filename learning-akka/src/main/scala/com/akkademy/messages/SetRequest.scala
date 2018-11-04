813c08719b332bdeb686e20cc9c06dc9147b1229
Youjing Zhu <chris0103@126.com>
2018-11-04 23:13:27 +0800

    Updated study files.

--- learning-akka/src/main/scala/com/akkademy/messages/SetRequest.scala
+++ learning-akka/src/main/scala/com/akkademy/messages/SetRequest.scala
@@ -1,5 +1,5 @@
 package com.akkademy.messages
 
-case class SetRequest(key : String, value : String)
+case class SetRequest(key : String, value : Object)
 case class GetRequest(Key : String)
 case class KeyNotFoundException(key : String) extends Exception

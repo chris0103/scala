80201ad0ed356f9db1adf0d3fed9b85d10ddd027
Chris Zhu <CZhu30@its.jnj.com>
2019-01-03 23:48:09 +0800

    Added messages.

--- learning-akka/akkademy-db/src/main/scala/com/akkademy/messages/Messages.scala
+++ learning-akka/akkademy-db/src/main/scala/com/akkademy/messages/Messages.scala
@@ -1,5 +1,7 @@
 package com.akkademy.messages
 
 case class SetRequest(key : String, value : Object)
+case class SetIfNotExists(key : String, value : Object)
 case class GetRequest(Key : String)
+case class Delete(Key : String)
 case class KeyNotFoundException(key : String) extends Exception

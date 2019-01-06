7ff067ed2084ab0c115c3141a57fa1d886dbc565
Youjing Zhu <chris0103@126.com>
2019-01-07 00:51:38 +0800

    Updated client test case.

--- learning-akka/akkademy-db-client/src/test/scala/com/akkademy/SClientIntegrationSpec.scala
+++ learning-akka/akkademy-db-client/src/test/scala/com/akkademy/SClientIntegrationSpec.scala
@@ -1,6 +1,8 @@
 package com.akkademy
 
+import com.akkademy.messages.KeyNotFoundException
 import org.scalatest.{FunSpecLike, Matchers}
+
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -13,10 +15,21 @@ class SClientIntegrationSpec extends FunSpecLike with Matchers {
       it("should set a value") {
         client.set("123", new Integer(123))
         val futureResult = client.get("123")
-        val result = Await.result(futureResult, 10 seconds)
+        val result = Await.result(futureResult, 2 seconds)
         result should equal(123)
       }
+
+      it("should fail on missing key") {
+        import scala.concurrent.ExecutionContext.Implicits.global
+
+        client.set("123", new Integer(123))
+        val future = client.get("321").recover {
+          case t : KeyNotFoundException => constructErrorMessage(t)
+        }
+        val result = Await.result(future, 2 seconds)
+        result should equal(constructErrorMessage(KeyNotFoundException("321")))
+      }
   }
+
+  def constructErrorMessage(t: KeyNotFoundException) : String = s"${t.getClass}[${t.key}]"
 }
-
-

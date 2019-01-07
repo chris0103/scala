926382cd2640616ea0e06fad6e8117c6e9b6b117
Youjing Zhu <chris0103@126.com>
2019-01-08 00:53:01 +0800

    Studying with reformat.

--- learning-akka/akkademaid/src/main/scala/com/akkademy/AskDemoArticleParser.scala
+++ learning-akka/akkademaid/src/main/scala/com/akkademy/AskDemoArticleParser.scala
@@ -1,22 +1,20 @@
 package com.akkademy
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorSelection}
 import akka.pattern.ask
 import akka.util.Timeout
-import com.akkademy.messages.{SetRequest, GetRequest}
+import com.akkademy.messages.{GetRequest, SetRequest}
 
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
-class AskDemoArticleParser(cacheActorPath: String,
-                           httpClientActorPath: String,
-                           acticleParserActorPath: String,
-                           implicit val timeout: Timeout
-                            ) extends Actor {
-  val cacheActor = context.actorSelection(cacheActorPath)
-  val httpClientActor = context.actorSelection(httpClientActorPath)
-  val articleParserActor = context.actorSelection(acticleParserActorPath)
+class AskDemoArticleParser(cacheActorPath: String, httpClientActorPath: String, acticleParserActorPath: String, implicit val timeout: Timeout) extends Actor {
+
+  val cacheActor: ActorSelection = context.actorSelection(cacheActorPath)
+  val httpClientActor: ActorSelection = context.actorSelection(httpClientActorPath)
+  val articleParserActor: ActorSelection = context.actorSelection(acticleParserActorPath)
+
   import scala.concurrent.ExecutionContext.Implicits.global
-
 
   /**
    * Note there are 3 asks so this potentially creates 6 extra objects:
@@ -29,29 +27,26 @@ class AskDemoArticleParser(cacheActorPath: String,
       val senderRef = sender() //sender ref needed for use in callback (see Pipe pattern for better solution)
 
       val cacheResult = cacheActor ? GetRequest(uri) //ask cache actor
-
       val result = cacheResult.recoverWith { //if request fails, then ask the articleParseActor
         case _: Exception =>
           val fRawResult = httpClientActor ? uri
-
           fRawResult flatMap {
             case HttpResponse(rawArticle) =>
               articleParserActor ? ParseHtmlArticle(uri, rawArticle)
-            case x =>
+            case _ =>
               Future.failed(new Exception("unknown response"))
           }
       }
 
-      // take the result and pipe it back to the actor
-      // (see Pipe pattern for improved implementation)
+      // take the result and pipe it back to the actor (see Pipe pattern for improved implementation)
       result onComplete {
-        case scala.util.Success(x: String) =>
+        case Success(x: String) =>
           println("cached result!")
           senderRef ! x //cached result
-        case scala.util.Success(x: ArticleBody) =>
+        case Success(x: ArticleBody) =>
           cacheActor ! SetRequest(uri, x.body)
           senderRef ! x
-        case scala.util.Failure(t) =>
+        case Failure(t) =>
           senderRef ! akka.actor.Status.Failure(t)
         case x =>
           println("unknown message! " + x)

f63423abd66e9cf85eead8e0c64d9d36498b9a1b
Youjing Zhu <chris0103@126.com>
2019-02-18 00:10:38 +0800

    Update DeliveryActor.scala

--- learning-akka/homework/src/main/scala/homework/chap02/DeliveryActor.scala
+++ learning-akka/homework/src/main/scala/homework/chap02/DeliveryActor.scala
@@ -2,20 +2,37 @@ package homework.chap02
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.util.Timeout
+import akka.pattern.ask
 import pong.ScalaPongActor
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
+
+import scala.concurrent.ExecutionContextExecutor
 
 class DeliveryActor extends Actor with ActorLogging {
 
   implicit val timeout: Timeout = Timeout(5 seconds)
+  implicit val ec: ExecutionContextExecutor = context.dispatcher
 
   val pongActor : ActorRef = context.actorOf(Props[ScalaPongActor])
+
+  val service : StringReverseService = new StringReverseService()
 
   override def receive: Receive = {
     case "Ping" =>
       log.info("Received {}.", "Ping")
-      val sender = sender()
-
+      val senderActor: ActorRef = sender()
+      askPong("Ping").onSuccess({
+        case msg => senderActor ! msg
+      })
+    case msg : String =>
+      log.info("Received {}.", msg)
+      val senderActor: ActorRef = sender()
+      senderActor ! service.reverseString(msg)
+    case x =>
+      println("unknown message " + x.getClass)
   }
+
+  def askPong(message: String) : Future[String] = (pongActor ? message).mapTo[String]
 }
